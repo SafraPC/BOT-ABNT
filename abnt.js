056e154684f0a44f9d4a3f76bef0aa8e67c5fb62
Leandro Safra <59981353+SafraPC@users.noreply.github.com>
2020-12-03 18:55:09 -0300

    init new side to get informations

--- abnt.js
+++ abnt.js
@@ -8,25 +8,47 @@ async function abnt(){
   //Main function that will run the project
 
   //Put some functions over here for get the data
-  async function getList(page){
-  const totalEvaluate = await page.evaluate(()=>{
-    const lis = document.querySelectorAll('.rpRootGroup li');
-    console.log([].map.call(lis,(li)=>li.textContent));
-    const list = [].map.call(lis,(li)=>li.textContent);
+
+  // async function getList(page){
+  // const totalEvaluate = await page.evaluate(()=>{
+  //   const lis = document.querySelectorAll('.rpRootGroup li');
+  //   console.log([].map.call(lis,(li)=>li.textContent));
+  //   const list = [].map.call(lis,(li)=>li.textContent);
+  //   return list;
+  //  });
+  // return totalEvaluate;
+  // }
+
+
+  async function getList(page,selector){
+    const list = await page.$$eval(selector, links => links.map(link => link.innerText))
     return list;
-   });
-  return totalEvaluate;
-  }
+   }
+   async function clickElement(page,selector){
+     const click = await page.$(selector);
+     if(click){
+       await click.click();
+   
+      }
+   }
+
     //Start a browser
     //Browser will open a new page
     //Page will go to a link.
     const browser = await puppeteer.launch({headless:false})
     const page  = await browser.newPage();
     await page.goto(INITIAL_URL);
-    const list = await getList(page)
-    console.log(list);
-
     
+    
+    
+   
+    //Onllist macrosetor labels
+    //Can use a,span, ul, li
+    const listDiv = await getList(page,'#tabs-1')
+   //List macrosetor labels and what have inside him
+     await clickElement(page,'#tabs-1');
+   console.log(listDiv);
+
 
 
 // browser.close()

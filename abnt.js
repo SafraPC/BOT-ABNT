c6e507bf9534fa6345821c11dc040e50488d777e
SafraPC <59981353+SafraPC@users.noreply.github.com>
2020-12-03 10:55:04 -0300

    Start to get some itens in ABNT

--- abnt.js
+++ abnt.js
@@ -3,17 +3,33 @@
 const puppeteer = require("puppeteer");
 const INITIAL_URL = "https://www.abntonline.com.br/consultanacional/default.aspx";
 
+//rpItem  classList that we need to open and click in the element
 async function abnt(){
-const browser = await puppeteer.launch({
-  headless: false,
-})
-const page  = await browser.newPage();
-await page.goto(INITIAL_URL);
+  //Main function that will run the project
+
+  //Put some functions over here for get the data
+  async function getList(page){
+  const totalEvaluate = await page.evaluate(()=>{
+    const lis = document.querySelectorAll('.rpRootGroup li');
+    console.log([].map.call(lis,(li)=>li.textContent));
+    const list = [].map.call(lis,(li)=>li.textContent);
+    return list;
+   });
+  return totalEvaluate;
+  }
+    //Start a browser
+    //Browser will open a new page
+    //Page will go to a link.
+    const browser = await puppeteer.launch({headless:false})
+    const page  = await browser.newPage();
+    await page.goto(INITIAL_URL);
+    const list = await getList(page)
+    console.log(list);
+
+    
+
+
+// browser.close()
 }
-
-abnt();
-
-
-
 module.exports = abnt;
 
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const ig = require("./abnt");
 
 (async () => {
-	await ig.init();
+	await ig();
 	debugger;
 })();

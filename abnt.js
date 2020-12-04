603f19656dc6f82b9d0d6cb0ede6bb8c1368d2c0
SafraPC <59981353+SafraPC@users.noreply.github.com>
2020-12-04 11:36:29 -0300

    click is ok, we need to get what have into them

--- abnt.js
+++ abnt.js
@@ -2,71 +2,53 @@
 //COMITÊS TÉCNICOS http://www.abnt.org.br/normalizacao/comites-tecnicos
 const puppeteer = require("puppeteer");
 const INITIAL_URL =
-  "https://www.abntonline.com.br/consultanacional/default.aspx";
+	"https://www.abntonline.com.br/consultanacional/default.aspx";
 
 //rpItem  classList that we need to open and click in the element
 async function abnt() {
-  //Main function that will run the project
+	//Main function that will run the project
 
-  //Put some functions over here for get the data
+	//Put some functions over here for get the data
+	const macrossetores = [];
+	async function getList(page) {
+		const macros = await page.evaluate((macrossetores) => {
+			const lists = document.querySelectorAll("li.rpItem");
+			const lis = document.querySelectorAll(".rpItem .rpExpandable");
+			lis.forEach((link) => {
+        
+        console.log(link);
+        link.click();
+				// // const as = document.querySelector(`${link} a`);
+				// console.log(as);
+				
+				// macrossetores.push({ macrossetor: link.innerText });
+			});
+			return macrossetores;
+		}, macrossetores);
+    console.log(macros);
+		return macros;
+	}
 
-  /*
-    async function getList(page) {
-    const totalEvaluate = await page.evaluate(() => {
-      const lis = document.querySelectorAll(".rpItem a.rpExpandable");
-      const list = [].slice.call(lis, 0, 12);
-      list.forEach(link =>{
-        console.log(link.classList[2].toString());
-        const teste = document.querySelectorAll(`${link.classList[2]}`);
-        console.log(teste);
-      }
-      );
-    });
-    //  await totalEvaluate.map(link => {
-    //     console.log(link);
-    //  });
-  }
-  */ 
+	async function clickElement(page, selector) {
+		const click = await page.$(selector);
+		await click.click();
+	}
 
-  async function getList(page) {
-    const totalEvaluate = await page.evaluate(() => {
-      const lis = document.querySelectorAll(".rpItem a.rpExpandable");
-      const list = [].slice.call(lis, 0, 12);
-      const teste = list.map(link =>{
-        link
-      });
-      return teste;
-    });
-    console.log(await totalEvaluate);
-    //  await totalEvaluate.map(link => {
-    //     console.log(link);
-    //  });
-  }
+	//Start a browser
+	//Browser will open a new page
+	//Page will go to a link.
+	const browser = await puppeteer.launch({ headless: false });
+	const page = await browser.newPage();
+	await page.goto(INITIAL_URL);
 
-  // async function getList(page,selector){
-  //   const list = await page.$$eval(selector, links => links.map(link => link.innerText))
-  //   list.splice(12,list.lenght-1);
-  //   return list;
-  //  }
-  async function clickElement(page, selector) {
-    const click = await page.$(selector);
-    await click.click();
-  }
+	//Only list macrosetor labels
+	//Can use:  a,span, ul, li
+	// const listDiv = await getList(page,'#tabs-1 a')
+   await getList(page);
+   console.log(macro);
+	//List macrosetor labels and what have inside him
+	//  await clickElement(page,'#tabs-1 a');
 
-  //Start a browser
-  //Browser will open a new page
-  //Page will go to a link.
-  const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
-  await page.goto(INITIAL_URL);
-
-  //Only list macrosetor labels
-  //Can use:  a,span, ul, li
-  // const listDiv = await getList(page,'#tabs-1 a')
-  await getList(page);
-  //List macrosetor labels and what have inside him
-  //  await clickElement(page,'#tabs-1 a');
-
-  // browser.close()
+	// browser.close()
 }
 module.exports = abnt;

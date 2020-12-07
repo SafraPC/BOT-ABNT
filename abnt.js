8ff3e14557b1275ae7bbf0aef2823748b561d406
SafraPC <59981353+SafraPC@users.noreply.github.com>
2020-12-07 11:10:35 -0300

    It´s almost there

--- abnt.js
+++ abnt.js
@@ -7,57 +7,31 @@ const INITIAL_URL =
 //rpItem  classList that we need to open and click in the element
 async function abnt() {
   //Main function that will run the project
-
   //Put some functions over here for get the data
-  const macrossetores = [];
-  const projetoMacrossetores = [];
-  const mesclaMacrosetores = [];
+
   async function getList(page) {
-    await page.evaluate(
-      (macrossetores, projetoMacrossetores, mesclaMacrosetores) => {
-        const lis = document.querySelectorAll(".rpItem .rpExpandable");
-        //path ".rpSlide >.rpGroup > .rpLevel1 > .rpItem > .rpFirst > .rpLast > .rpLink > .rpOut > .rpText"
-        // const lisParents = document.querySelectorAll(".rpSlide");
-        const lisParents = document.querySelectorAll(".rpRootGroup > li");
-        lis.forEach((link) => {
-          link.click();
-          macrossetores.push({
-            macrossetor: link.textContent,
-          });
-        });
-        lisParents.forEach((liParent) => {
+    const macrossetores = await page.evaluate(() => {
+      const macrossetores = [];
+      const lisParents = document.querySelectorAll(".rpRootGroup > li");
 
-		const teste = [].map.call(
-				liParent.children[1].children[0].children,
-				(child) => child.innerText
-			  );
-
-          projetoMacrossetores.push({
-            comites: teste,
-            
-          });
-			
-	});
-        for (let i = 0; i < macrossetores.length; ) {
-          mesclaMacrosetores.push({
-            macrossetor: macrossetores[i],
-            projeto: projetoMacrossetores[i],
-          });
-          i++;
-        }
-        console.log(mesclaMacrosetores);
-      },
-      macrossetores,
-      projetoMacrossetores,
-      mesclaMacrosetores
-    );
+      lisParents.forEach((liParent) => {
+        console.log(
+          [].map.call(
+            liParent.children[1].children[0].children,
+            (child) => child.innerText
+          )
+        );
+        // macrossetores.push({
+        //   title: liParent.children[0].innerText,
+        //   comite: [].map.call(
+        //     liParent.children[1].children[0].children,
+        //     (child) => child.innerText
+        //   ),
+        // });
+      });
+      return macrossetores;
+    });
   }
-
-  async function clickElement(page, selector) {
-    const click = await page.$(selector);
-    await click.click();
-  }
-
   //Start a browser
   //Browser will open a new page
   //Page will go to a link.

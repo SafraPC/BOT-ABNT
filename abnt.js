21db89a718838acaba1cb56543ee1287ecb2605a
Leandro Safra <59981353+SafraPC@users.noreply.github.com>
2020-12-11 20:50:50 -0300

    can get all data just need to trate the data

--- abnt.js
+++ abnt.js
@@ -21,38 +21,35 @@ async function abnt() {
     //Try to get project´s by all project page
 
     const getProjectsFunc = await secondary_page.evaluate(async () => {
-      
-     
       const getOrderClick = document.querySelector(
         "#ctl00_ContentPlaceHolder1_gridProjeto_ctl00 > thead > tr > th:nth-child(4) > a"
       );
       await getOrderClick.click();
-  
+
       //get projects
-       
-          let abntProjects = [
-            ...document.querySelectorAll(
-              "#ctl00_ContentPlaceHolder1_gridProjeto_ctl00 > tbody > tr"
-            ),
-          ];
-        const getProjects = [];
-        abntProjects.forEach((project) => {
-              getProjects.push({
-                macrossetor: project.children[3].innerText,
-                comite: project.children[2].innerText,
-                projeto:({titulo:project.children[1].innerText,
-                  numero:project.children[0].innerText,
-                  data:project.children[4].innerText,
-                  link:project.children[1].children[0].href})
-                }, 
-              )
+
+      let abntProjects = [
+        ...document.querySelectorAll(
+          "#ctl00_ContentPlaceHolder1_gridProjeto_ctl00 > tbody > tr"
+        ),
+      ];
+      const getProjects = [];
+      abntProjects.forEach((project) => {
+        getProjects.push({
+          macrossetor: project.children[3].innerText,
+          comite: project.children[2].innerText,
+          projeto: {
+            titulo: project.children[1].innerText,
+            numero: project.children[0].innerText,
+            data: project.children[4].innerText,
+            link: project.children[1].children[0].href,
+          },
+        });
       });
       console.log(getProjects);
       return getProjects;
-    
-   
     });
-  
+
     const macrossetoresFunc = await page.evaluate(async () => {
       const lisParents = [
         ...document.querySelectorAll("#tabs-1 .rpRootGroup > li"),
@@ -69,9 +66,9 @@ async function abnt() {
       });
       return macrossetores;
     });
-      console.log(getProjectsFunc);
-      console.log(macrossetoresFunc);
-      console.log("Projetos Ativos :"+getProjectsFunc.length);
+    console.log(getProjectsFunc);
+    console.log(macrossetoresFunc);
+    console.log("Projetos Ativos :" + getProjectsFunc.length);
   }
 
   //Start a browser
